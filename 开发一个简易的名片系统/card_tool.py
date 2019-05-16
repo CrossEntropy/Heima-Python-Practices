493eb064f8261e25de480b8ff53a6680e881551b
Panda <931214091@qq.com>
2019-05-16 11:26:25 +0800

    重构变量名，采用PyChram重构变量名功能： shift + F6

--- 开发一个简易的名片系统/card_tool.py
+++ 开发一个简易的名片系统/card_tool.py
@@ -31,35 +31,43 @@ def new_card():
     print("新增名片")
 
     # 1. 提示用户输入名片的详细信息
-    name = input("请输入姓名： ")
-    phone = input("请输入电话号码： ")
-    qq = input("请输入QQ: ")
-    email = input("请输入email: ")
+    name_str = input("请输入姓名： ")
+    phone_str = input("请输入电话号码： ")
+    qq_str = input("请输入QQ: ")
+    email_str = input("请输入email: ")
 
     # 2. 使用用户输入的信息建立一个名片的字典
-    card_dict = {"姓名": name,
-              "电话": phone,
-              "qq": qq,
-              "email": email}
+    card_dict = {"姓名": name_str,
+              "电话": phone_str,
+              "qq_str": qq_str,
+              "email_str": email_str}
     # 3. 将名片字典添加到列表中
     card_list.append(card_dict)
-    
+
     # 4. 提示用户添加成功
     print("添加成功")
     print("*" * 40)
 
 
 def show_all_card():
-    """"""
+    """显示所有名片"""
+    # card_list为空
+    if len(card_list) == 0:
+        print("*" * 40)
+        print("当前名片中没有任何用户信息！！！请添加！！！")
+        return None
+
+    # card_list不为空
     print("*" * 40)
     print("显示所有名片")
 
 
-def search_card():
-    """"""
+def search_card(name):
+    """寻找用户信息"""
+
     print("*" * 40)
     print("搜索名片")
 
 
 if __name__ == "__main__":
-    show_menu()
+    new_card()
