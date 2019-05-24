3b02c4c3eee924d58f21749ab64a5ab26bf535f1
Panda <931214091@qq.com>
2019-05-24 14:14:09 +0800

    更新README.md

--- 方法综合案例/game_main.py
+++ 方法综合案例/game_main.py
@@ -5,16 +5,23 @@ class Game(object):
 
     top_score = 0
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, player_name):
+        self.player_name = player_name
 
     @staticmethod
     def show_help():
-        pass
+        print("帮助信息: 让僵尸进入大门")
 
     @classmethod
     def show_top_score(cls):
         print("当前历史最高分为: %.2f" % cls.top_score)
 
     def start_name(self):
-        pass
+        print("%s 开始游戏啦..." % self.player_name)
+
+
+if __name__ == "__main__":
+    Game.show_help()
+    Game.show_top_score()
+    game = Game("小明")
+    game.start_name()
